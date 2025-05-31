04240692b852d1b63bb9de9d2f72c253f03741ec
Nezz77 <nesanduhm@gmail.com>
2025-05-31 13:23:44 +0530

    updated authcontroller

--- src/main/java/lk/ijse/cmjd109/LostandFoundSystem/controller/secure/AuthController.java
+++ src/main/java/lk/ijse/cmjd109/LostandFoundSystem/controller/secure/AuthController.java
@@ -3,6 +3,7 @@ package lk.ijse.cmjd109.LostandFoundSystem.controller.secure;
 import lk.ijse.cmjd109.LostandFoundSystem.dto.secure.JWTAuthResponse;
 import lk.ijse.cmjd109.LostandFoundSystem.dto.secure.SignIn;
 import lk.ijse.cmjd109.LostandFoundSystem.dto.secure.UserDTO;
+import lk.ijse.cmjd109.LostandFoundSystem.service.secure.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    @PostMapping
+    private final AuthService authService;
+    @PostMapping("signin")
     public ResponseEntity<JWTAuthResponse> signIn(@RequestBody SignIn signIn) {
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(authService.signIn(signIn),HttpStatus.OK);
     }
-    @PostMapping
-    public ResponseEntity<JWTAuthResponse> signUp(@RequestBody UserDTO signUp) {
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+    @PostMapping("signup")
+    public ResponseEntity<JWTAuthResponse> signUp(@RequestBody UserDTO userDTO) {
+        return new ResponseEntity<>(authService.signUp(userDTO), HttpStatus.OK);    }
 }
