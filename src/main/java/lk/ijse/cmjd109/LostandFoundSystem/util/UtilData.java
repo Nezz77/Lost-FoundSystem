53a7035aeee9dc0bfc59b591a852f7b05c32e266
Nezz77 <nesanduhm@gmail.com>
2025-06-05 15:02:28 +0530

    change auto generating id length

--- src/main/java/lk/ijse/cmjd109/LostandFoundSystem/util/UtilData.java
+++ src/main/java/lk/ijse/cmjd109/LostandFoundSystem/util/UtilData.java
@@ -9,13 +9,13 @@ import static java.time.LocalTime.now;
 
 public class UtilData {
     public static String generateItemId(){
-        return "I-"+ UUID.randomUUID();
+        return "I-"+ UUID.randomUUID().toString().substring(0, 8);
     }
     public static String generateRequestId(){
-        return "R-"+ UUID.randomUUID();
+        return "R-"+ UUID.randomUUID().toString().substring(0, 8);
     }
     public static String generateUserId(){
-        return "U-"+ UUID.randomUUID();
+        return "U-"+ UUID.randomUUID().toString().substring(0, 8);
     }
 
     ///generate requested date and time
